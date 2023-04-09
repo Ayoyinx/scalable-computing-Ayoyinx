5174be9b719b27538e11356474fc540f0ea3d7a6
toluhunter <toluhunter19@gmail.com>
2023-04-09 15:56:22 +0100

    Added Google Oauth in settings.py

--- walletapi/settings.py
+++ walletapi/settings.py
@@ -150,3 +150,6 @@ OAUTH2_PROVIDER = {
     # this is the list of available scopes
     'SCOPES': {'deduct amount': 'Ability to withdraw amount from account'}
 }
+GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET"),
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
+GOOGLE_REDIRECT_URL = os.getenv("GOOGLE_REDIRECT_URL")
